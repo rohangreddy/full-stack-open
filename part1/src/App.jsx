681e299f2a5770704a3ee23b7c30b96246fb9c36
Rohan Reddy <rohangreddy@gmail.com>
2026-05-21 18:25:49 -0400

    init

---
+++ part1/src/App.jsx
@@ -0,0 +1,57 @@
+// Define componenet that logs to console and then returns html div element with hello world paragraph
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App
+  
+//{
+//   const course = 'Half Stack Application Development'
+//   const part1 = {
+//     name: 'Fundamentals of React',
+//     exercises: 10
+//   }
+//   const part2 = {
+//     name: 'Using props to pass data',
+//     exercises: 7
+//   }
+//   const part3 = {
+//     name: 'State of a component',
+//     exercises: 14
+//   }
+
+//   const parts = [part1, part2, part3]
+
+//   return (
+//     <div>
+//       <Header course={course}/>
+//       <Content parts={parts}/>
+//       <Total parts={parts} />
+//     </div>
+//   )
+// }
+
+// export default App
