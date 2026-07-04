92dc892ac15b11f80cc0f2fae18b059adb3681c2
Rohan <rohanreddy34@gmail.com>
2026-07-04 09:33:59 -0400

    part2c effects

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,5 +1,6 @@
 import Note from './components/Note'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = (props) => {
 
@@ -19,7 +20,33 @@ const App = (props) => {
   // This becomes important if your array items can move (e.g. due to sorting), get inserted, or get deleted. 
   // A well-chosen key helps React infer what exactly has happened, and make the correct updates to the DOM tree.
 
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
+
+  // Effects let a component connect to and synchronize with external system
+  // By default, effect is always run after the component has been rendered
+  const hook = () => {
+    console.log('effect')
+    // Axios get method returns a promise
+    // A promise is an object representing the eventual completion or failure of an asynchronous operation
+    // A promise can generally have 3 states: pending, fulfilled, rejected
+    // This block initiates fetching of data from the server and then registers an event handler
+    // such that when the data arrives from the server, JS runtime calls the function registered as the event handler
+    // and stores the notes received rom the server into the state using setNotes
+    // Finally, a call to the state-updating function triggers re-rendering of the component
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+
+  // first parameter is a function (effect)
+  // second parameter specifies how often the effect should run (if [], effect is only run with the first render of the component)
+  useEffect(hook, [])
+    
+  console.log(`rendered ${notes.length} notes`)
+
   // Controlled components are a way to access data contained in the form's input element
   // In the first iteration where we set newNote to input value, React doesn't allow edits because we haven't defined what to do when the input changes
   const [newNote, setNewNote] = useState(
